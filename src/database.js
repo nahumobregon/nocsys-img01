2410f755b570f4ad1ff9ae1c96ccb9996d00a645
Nahum Obregon <nahumobregon@gmail.com>
2019-09-18 03:47:58 -0500

    Modificando el archivo database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('process.env.MONGODB_URI || mongodb://localhost/finterest_tutorial', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/finterest_tutorial', {
 	//userNewUrlParser: true
 	useCreateIndex: true ,
 	useNewUrlParser: true ,
